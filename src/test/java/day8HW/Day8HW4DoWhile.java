bd6605f71a57605bde4d6f975b547c91e3a22fde
GKM <GKM@GKMLAPTOP>
2022-01-01 12:56:27 -0500

    Day8HW5

--- src/test/java/day8HW/Day8HW4DoWhile.java
+++ src/test/java/day8HW/Day8HW4DoWhile.java
@@ -1,5 +1,37 @@
 package day8HW;
 
 public class Day8HW4DoWhile {
+	
+public static void main(String[] args) {
+		
+		int evenTotal = 0;
+		
+		int oddTotal = 0;
+		
+		int count = 1;
+		
+		do {	
+			if (count % 2 == 0) {
+				
+				evenTotal = evenTotal + count;
+				
+			} else {
+				
+				oddTotal = oddTotal +  count;
+			
+			}
+			count++;
+			
+			} while (count <= 20);
+		
+		
+		System.out.println("Do While Even Total: " + evenTotal);
+		System.out.println("Do While Odd Total: " + oddTotal);
+
+
+
+	}
 
 }
+
+
