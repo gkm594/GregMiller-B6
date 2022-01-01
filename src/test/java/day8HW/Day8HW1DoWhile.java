01b16cfd37e3ced14c840f101026b02abffea18e
GKM <GKM@GKMLAPTOP>
2022-01-01 12:49:25 -0500

    Day8HW4For

--- src/test/java/day8HW/Day8HW1DoWhile.java
+++ src/test/java/day8HW/Day8HW1DoWhile.java
@@ -1,5 +1,32 @@
 package day8HW;
 
 public class Day8HW1DoWhile {
+	
+	public static void main(String[] args) {
+
+		int i = 1;
+		do {
+	
+
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("CozaLoza");
+
+			} else if (i % 3 == 0) {
+				System.out.println("Coza");
+
+			} else if (i % 5 == 0) {
+				System.out.println("Loza");
+
+			} else if (i % 7 == 0) {
+				System.out.println("Woza");
+
+			} else {
+				System.out.println(i);
+			}	
+				i++;
+			
+			} while (i <= 36);
+
+	}
 
 }
--- src/test/java/day8HW/Hurricane.java
+++ src/test/java/day8HW/Hurricane.java
@@ -2,4 +2,36 @@ package day8HW;
 
 public class Hurricane {
 
+public static void main(String[] args) {
+		
+		int i = 20;
+		
+		while(i <= 200) {
+			
+			if(i >=74 && i <=95) {
+				System.out.println("Category 1 Hurricane: " + i);
+				i++;
+				 
+			} else if(i >= 105 && i <= 110) {
+				System.out.println("Category 2 Hurricane: " + i);
+				i++;
+				
+			} else if(i >= 120 && i <= 130) {
+				System.out.println("Category 3 Hurricane: " + i);
+				i++;
+				
+			} else if(i >= 140 && i <= 155) {
+				System.out.println("Category 4 Hurricane: " + i);
+				i++;
+				
+			} else if(i >= 165 && i <= 194) {
+				System.out.println("Category 5 Hurricane: " + i);
+				i++;
+				
+			} else {
+				System.out.println("Not a hurricane: " + i);
+				i++;
+			}
+		}
+}
 }
